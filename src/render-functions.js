c6a90cbec9b860097339a4f4cd592919ced3d377
RyJaxxon <ryanjaxxon@gmail.com>
2024-06-19 23:03:57 -0400

    completed 4-8

--- src/render-functions.js
+++ src/render-functions.js
@@ -30,14 +30,43 @@ export const setupPageBasics = (parentEl) => {
     return { statusDiv, usersUl, postsUl, newUserForm, newUserDiv };
 };
 
-export const renderStatus = () => {
-}
-
-export const renderUsers = () => {
+export const renderStatus = (statusDiv, statusInfoObj) => {
+  const htmlContent = `
+  <h2 id='status-heading'>Info on - ${statusInfoObj.url}</h2>
+  <p id='status-code'>Status code: ${statusInfoObj.status}, ${statusInfoObj.ok ? 'OK!' : 'FAIL!'}</p>
+  `;
+  statusDiv.innerHTML = htmlContent
 };
 
-export const renderPosts = () => {
+export const renderUsers = (usersUl, users) => {
+  let htmlContent = ''
+  users.forEach((user) => {
+    htmlContent += `
+    <li class ="user-card">
+      <button data-user-id=${user.id}>Load ${user.username}'s posts</button>
+    </li>
+    `;
+  })
+  usersUl.innerHTML = htmlContent;
 }
 
-export const renderNewUser = () => {
-}
+export const renderPosts = (postsUl,posts) => {
+  let htmlContent = ''
+  posts.forEach((post) => {
+    htmlContent += `
+    <li>
+      <h2>${post.title}</h2>
+      <p>${post.body}</p>
+    </li>
+    `;
+  })
+  postsUl.innerHTML = htmlContent;
+};
+
+export const renderNewUser = (newUserDiv,newUserInfo) => {
+  newUserDiv.innerHTML = ``;
+  newUserDiv.innerHTML = `
+  <h2>${newUserInfo.username}</h2>
+  <p>${newUserInfo.email}</p>
+  `;
+};
